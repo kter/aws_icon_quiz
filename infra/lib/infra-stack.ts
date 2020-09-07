1d4a23a9808e5706df7fd2c68e34f951f7d8cf99
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-09-07 21:07:08 +0900

    修正

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigateway from "@aws-cdk/aws-apigateway";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -32,8 +33,54 @@ export class InfraStack extends cdk.Stack {
     // dynamodb読み取り権限をLambdaに付与
     dynamoTable.grantReadData(getItemLambda);
 
-    const api = new RestApi(this, "itemsApi", {
+    const api = new apigateway.RestApi(this, "itemsApi", {
       restApiName: "Items Service",
     });
+
+    const items = api.root.addResource("items");
+
+    const singleItem = items.addResource("{id}");
+    const getItemIntegration = new apigateway.LambdaIntegration(getItemLambda);
+    singleItem.addMethod("GET", getItemIntegration);
+    addCorsOptions(items);
   }
+}
+
+export function addCorsOptions(apiResource: apigateway.IResource) {
+  apiResource.addMethod(
+    "OPTIONS",
+    new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers":
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+            "method.response.header.Access-Control-Allow-Credentials":
+              "'false'",
+            "method.response.header.Access-Control-Allow-Methods":
+              "'OPTIONS,GET,PUT,POST,DELETE'",
+          },
+        },
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": '{"statusCode": 200}',
+      },
+    }),
+    {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers": true,
+            "method.response.header.Access-Control-Allow-Methods": true,
+            "method.response.header.Access-Control-Allow-Credentials": true,
+            "method.response.header.Access-Control-Allow-Origin": true,
+          },
+        },
+      ],
+    }
+  );
 }

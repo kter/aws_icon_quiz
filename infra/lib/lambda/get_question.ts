410c8f76eddadc4a2a9f58110e8612a9432cceb3
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-09-27 11:16:13 +0900

    とりあえずscan結果をlogに出す

--- infra/lib/lambda/get_question.ts
+++ infra/lib/lambda/get_question.ts
@@ -1,28 +1,19 @@
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME || "";
-const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
+const CHOICES_NUM = process.env.CHOICES_NUM || "4";
+const QUESTIONS_NUM = process.env.QUESTIONS_NUM || "20";
 
 export const handler = async (event: any = {}): Promise<any> => {
-  const requestedItemId = event.pathParameters.id;
-    if (!requestedItemId) {
-      return {
-        statusCode: 400,
-        body: `Error: You are missing the path parameter id`,
-      };
-    }
-
   const params = {
     TableName: TABLE_NAME,
-      Key: {
-        [PRIMARY_KEY]: requestedItemId,
-      },
-    };
+  };
 
   try {
-    const response = await db.get(params).promise();
-      return { statusCode: 200, body: JSON.stringify(response.Item) };
-    } catch (dbError) {
-      return { statusCode: 500, body: JSON.stringify(dbError) };
+    const response = await db.scan(params).promise();
+    console.log(JSON.stringify(response));
+    return { statusCode: 200, body: JSON.stringify(response.Item) };
+  } catch (dbError) {
+    return { statusCode: 500, body: JSON.stringify(dbError) };
   }
 };

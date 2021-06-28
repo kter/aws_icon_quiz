d80dd383e568dbc61ca395e7ffa299ef5b09d4bd
kter <me@kter.jp>
2021-06-29 00:07:05 +0900

    エラーに対応

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { InfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+new InfraStack(app, 'InfraStack', {
+    env: {
+        region: 'ap-northeast-1',
+        account: '848738341109',
+    }
+});
